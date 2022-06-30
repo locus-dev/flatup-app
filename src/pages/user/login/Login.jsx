de86a8c02e2add47f97af1e49b177f9da83b291c
LucasMiller98 <lucas.miller1998@outlook.com>
2022-06-30 11:45:25 -0300

    integração do login no front-end com o back com axios

--- src/pages/user/login/Login.jsx
+++ src/pages/user/login/Login.jsx
@@ -1,6 +1,22 @@
 import "./login.css";
+import React, {useState} from 'react';
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [email,setEmail] = useState("")
+  const [senha,setSenha] = useState("")
+
+  function logar(){
+    axios.post('http://localhost:8081/', data={"email":email,"senha":senha}).then(
+      navigate('/Home')
+    ).catch(error => {console.log('deu erro')})
+      
+  }
+
+
   return (
     <div>
       <main>
@@ -21,20 +37,23 @@ const Login = () => {
                   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                 </svg>
                 </span> <span>Facebook</span> </button>
+
             </div>
 
             <h3 className="or">Ou</h3>
             <div className="form-control">
               <label className="lsOptionText" for="email">Email </label>
-              <input type="email" min={0} className="input" placeholder="Email" id="email" />
+              <input type="email" min={0} className="input" placeholder="Email" id="email"  value={email} onChange={e => setEmail(e.target.value)} />
             </div>
             <div className="form-control">
               <label className="lsOptionText" for="senha">Senha</label>
-              <input type="password" min={0} className="input" placeholder="Senha" id="senha" />
+              <input type="password" min={0} className="input" placeholder="Senha" id="senha" value={senha} onChange={e => setSenha(e.target.value)}/>
             </div>
             <a href="/forgot" id="forgot-senha">Esqueci a senha</a>
 
-            <button type="submit" className="form-button">Entrar</button>
+            <button onClick={
+              logar()
+            } className="form-button" >Entrar</button>
             <p className="registre">Ainda não tem conta? <a href="/register">Registre-se</a></p>
           </div>
         </div>
