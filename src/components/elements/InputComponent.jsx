9d064b48619f3e763f3adfb133aee969d61c9627
pedro-ss <pedross.tool@gmail.com>
2022-08-01 23:04:36 -0300

    adicionando componentes de react bootstrap

--- src/components/elements/InputComponent.jsx
+++ src/components/elements/InputComponent.jsx
@@ -1,15 +1,21 @@
+import Form from 'react-bootstrap/Form';
 
 const InputComponent = (props) => {
-    return (
-        <div className={props.divClassname}>
-            <label>{props.label}</label>
-            <input 
-                type={props.type} 
-                className={props.className}
-                placeholder={props.placeholder} 
-                onChange={props.change}/>
-        </div>
-    )
+  return (
+    <>
+      <Form.Label htmlFor={props.inputId}>{props.inputName}</Form.Label>
+      <Form.Control
+          type={props.inputType}
+          id={props.inputId}
+          aria-describedby={props.inputDesc}
+          placeholder={props.placeholder} 
+          onChange={props.change}
+      />
+      <Form.Text id={props.inputDesc} muted>
+        {props.inputTip}
+      </Form.Text>
+    </>
+  );
 }
 
 export default InputComponent;
