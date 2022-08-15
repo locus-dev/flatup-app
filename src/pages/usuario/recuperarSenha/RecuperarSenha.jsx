62a5263619c85dc51b77d672942ad8b10b67a3b0
williams181 <william.roberto39@hotmail.com>
2022-08-15 17:26:30 -0300

    tela modificada para o padrão do projeto

--- src/pages/usuario/recuperarSenha/RecuperarSenha.jsx
+++ src/pages/usuario/recuperarSenha/RecuperarSenha.jsx
@@ -1,32 +1,91 @@
-import "./recuperarSenha.css";
-import Navbar from "../../../components/navbar/Navbar";
+// import API from "../../../services/API";
+import axios from "axios";
+import { initializeApp } from "firebase/app";
+import {
+	FacebookAuthProvider,
+	getAuth,
+	GoogleAuthProvider,
+	signInWithPopup,
+} from "firebase/auth";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../../../components/navbar/Navbar";
+import "./recuperarSenha.css";
+import FlatUpContext from "../../../components/context/FlatUpContext";
+import { useContext } from "react";
 
+// App.SettarToken("fnfnfn")
 const Forgot = ({navigate}) => {
+  
+	const [email, setEmail] = useState("");
+	const [senha, setSenha] = useState("");
 
   const recuperar = () => {
 		navigate("/", { state: {} });
 	};
 
-  return (
-    <div>
-      <main>
+	return (
+		<div>
 
-      <Navbar />
-        <div className="side-form">
-          <div className="form-box">
-            
-            <div className="form-control">
-              <h3>Esqueceu Senha</h3><br></br>
-              <label className="lsOptionText" for="email"> {" "} <input type="email" min={0} placeholder="Email" id="email" /> </label>
-              <button type="submit" onClick={recuperar} className="form-button">Recuperar</button>
-            </div>
-            
-          </div>
-        </div>
-      </main>
-    </div>
-  )
-}
+			<main>
+			{/* <Navbar /> */}
+				<div className="side-form">
+					<div className="form-box">
+						<h1>Recuperar Senha</h1>
 
-export default Forgot
+						{/* <form action={config.URL + '/auth'} method="POST"> */}
+						<div className="login-box d-flex flex-column">
+							<div>
+								<label className="lsOptionText" for="email">
+									{" Email: "}
+								</label>
+								<input
+									className="input-padrao w-100"
+									type="email"
+									min={0}
+									value={email}
+									name="email"
+									placeholder="Email"
+									id="email"
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</div>
+							<div>
+								<label className="lsOptionText" for="senha">
+									{" Senha: "}
+								</label>
+								<input 
+									className="input-padrao w-100"
+									type="password"
+									min={0}
+									value={senha}
+									name="senha"
+									placeholder="Senha"
+									id="senha"
+									onChange={(e) => setSenha(e.target.value)}/>
+							</div>							
+						</div>
+
+						<button
+							// type="submit"
+							className="form-button"
+							onClick={recuperar}
+						>
+							Enviar
+						</button>
+						{/* </form> */}
+			
+					</div>
+				</div>
+
+				<img
+					alt="Coqueiros"
+					src="./media/assets/image-login.jpg"
+					className="other-side"
+				/>
+			</main>
+		</div>
+	);
+};
+
+export default Forgot;
