6293f747165f342eca4b892ef5dca915e2bd4113
wendel isaac <wendel.isaac12@gmail.com>
2022-08-22 23:28:52 -0300

    toast de sucesso ao alugar imovel

--- src/pages/usuario/perfil/locacoes/MinhasLocacoes.jsx
+++ src/pages/usuario/perfil/locacoes/MinhasLocacoes.jsx
@@ -2,6 +2,8 @@ import Footer from "../../../../components/footer/Footer";
 import Navbar from "../../../../components/navbar/Navbar";
 import FeaturedProperties from "../../../../components/featuredProperties/FeaturedProperties";
 import { useLocation } from "react-router-dom";
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 import './MinhasLocacoes.css'
 import { useState } from "react";
 
@@ -15,12 +17,25 @@ const MinhasLocacoes = () => {
             <h2 className="mb-4">Histórico de imóveis</h2>
             <FeaturedProperties />
         </div>
-        <div style={{display: "hidden"}} id="sucesso">
-            {/* <span onClick={()=>{setLiga("")}} id="close">X</span> */}
+        {/* <div style={{display: "hidden"}} id="sucesso">
+            <span onClick={()=>{setLiga("")}} id="close">X</span> 
             <div>
             <h1>Reserva registrada</h1>
             <p>Sua reserva foi registrada com sucesso!</p>
             </div>
+        </div> */}
+        <div style={{display: "hidden",minHeight: '240px' }} id="sucesso"
+        aria-live="polite"
+        aria-atomic="true"
+        className="bg-success position-relative"
+      >
+            <ToastContainer className="p-3" position={'top-end'}>
+            <Toast>
+                <Toast.Header closeButton={false}>
+                </Toast.Header>
+                <Toast.Body>Sua reserva foi registrada com sucesso!</Toast.Body>
+            </Toast>
+            </ToastContainer>
         </div>
     </>
     )
